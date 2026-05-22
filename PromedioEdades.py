48ff996819aa1d594cdbedebf5809432ba1164dd
AndreaCarolinaRamirezM <andreita.ramirez0171@gmail.com>
2026-05-21 21:04:42 -0500

    Feat:"Adicion Funcion Calculo Por Salones"

--- PromedioEdades.py
+++ PromedioEdades.py
@@ -1,11 +1,14 @@
 def CalcularPromedioDeEdad():
-    Cantidad_Alumnos=int(input("digite la cantidad de alumnos"))
+    Cantidad_Salones=int(input("digite la cantidad de salones "))
     Suma_Edades=0
-    for i in range(1,Cantidad_Alumnos+1):
-        Edad= float(input("digite la edad del alumno"))
-        Suma_Edades=Suma_Edades+Edad
+    for j in range(1,Cantidad_Salones+1):
+        Cantidad_Alumnos=int(input("digite la cantidad de alumnos "))
+        for i in range(1,Cantidad_Alumnos+1):
+            Edad= float(input("digite la edad del alumno "))
+            Suma_Edades=Suma_Edades+Edad
 
     Promedio_Edad=Suma_Edades/Cantidad_Alumnos
+    print(Promedio_Edad)
 
 CalcularPromedioDeEdad()
 
